5486072e1ab7eb49faf1f16ff67861167e758b2d
Omer Farooq Ahmed <omerfarooqali@gmail.com>
2019-01-23 22:55:35 +0500

    first commit

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const functions = require("firebase-functions");
+const SENDGRID_API_KEY = functions.config().sendgrid.key;
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(SENDGRID_API_KEY);
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const app = express();
+app.get("/timestamp", (req, res) => {
+  res.send(`${Date.now()}`);
+  const msg = {
+    from: "quote@crescon.org",
+    templateId: "d-03b9053572554e1491f2bd97047e957b",
+    personalizations: [
+      {
+        to: [
+          {
+            email: "omerfarooqali@gmail.com"
+          }
+        ]
+      }
+    ]
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("quote email sent");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+exports.app = functions.https.onRequest(app);
